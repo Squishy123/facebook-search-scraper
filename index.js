2c5b931e8eb39ef2cfeda4daeb92884df7752ff0
Christian Wang <christian.gnaw@gmail.com>
2019-03-07 17:05:08 -0500

    Added browser and page closure and exported search

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const scraper = require('./scraper.js');
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 require('dotenv').config();
 
 (async () => {
     try {
      //launch browser
-    let browser = await puppeteer.launch({ headless: false });
+    let browser = await puppeteer.launch({ headless: true });
 
     //launch page
     let page = await browser.newPage();
@@ -15,10 +16,22 @@ require('dotenv').config();
     await scraper.login(browser, page,
         process.env.FB_USERNAME, process.env.FB_PASSWORD);
 
-    //search 
+    //search and get results
     let results = await scraper.search(browser, page, "john wick");
-    console.log(results);
+
+    //export results    
+    await fs.writeFile('search.json', JSON.stringify(results), (err, data) => {
+        if(err)
+            console.log(err);
+        console.log("Successfully written!");
+    });
+    
+    //close page and browser
+    await page.close();
+    await browser.close();
+
     } catch(err) {
         console.log(err);
     }
+    
 })();
